083093d99c6d28b42336193dd629a1e8cd670718
DecamLekgau <161304335+DecamLekgau@users.noreply.github.com>
2024-08-07 22:33:55 +0200

    Add files via upload

---
+++ chapter 9/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//*************HISTORY MODEL************* */
+
+// console.dir(window);
+// console.dir(window.history.length);
+// console.log(history);
+
+// //***********NAVIGATOR OBJECT************* */
+// console.dir(window.navigator);
+
+// //***********LOCATION OBJECT*********** */
+// console.dir(location);
+
+// //************THE DOM********** */
+// console.dir(document);
+
+// const e1 = document.querySelector("h1");
+// console.dir(e1);
+
+// const allE = document.querySelectorAll(".output");
+// console.log(allE);
+//**************************************** */
